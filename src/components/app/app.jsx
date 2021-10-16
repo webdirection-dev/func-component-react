b7d5e29b63be9426fd0f89df2b3a11952048f6a6
MacAir <itech.capitals@icloud.com>
2021-10-16 21:30:40 +0300

    ADD
    Added hook useState()

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,8 +1,43 @@
-import './app.css';
+import React, {useState} from "react";
 
-function App() {
+const App = () => {
+    // useState - из набораа хукков
+    // деструктуризация массива, возвращенного из useState()
+    // по умолчанию const value === 0, т.к useState(0) - т.е 1й аргумент функции useState это значение для value
+    const [value, setValue] = useState(11);
+    const [value2, setValue2] = useState(22);
+
+    // изменим value передав другое значение в аргумет useState(2)
+    // теперб value = 1
+    useState(33)
+
+
+    // Например счетчик
+    const [count, setCount] = useState(0);
+
+    const increment = () => {
+        setCount(count + 1)
+    };
+
+    const decrement = () => {
+        setCount(count - 1)
+    };
+
     return (
-        <h1>Test</h1>
+        <>
+            <button onClick={increment}>+</button>
+
+            <span
+                style={{
+                    display: 'inline-block',
+                    margin: '10px'
+                }}
+            >
+                {count}
+            </span>
+
+            <button onClick={decrement}>-</button>
+        </>
     );
 }
 
